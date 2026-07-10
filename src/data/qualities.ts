2df46c94d524380e3e7d589fc92074724d22b9f0
jotajotafv <fernandezvillazonjuanjose@gmail.com>
2026-07-10 02:50:39 -0400

    Cambiar textos de Su a Tu

--- src/data/qualities.ts
+++ src/data/qualities.ts
@@ -1,10 +1,10 @@
 export const qualities = [
-  "Su alegría",
-  "Su inteligencia",
-  "Su hermosa sonrisa",
-  "Su sentido del humor",
-  "Su mirada",
-  "Su forma de hablar y expresarse",
-  "Su ternura",
-  "Su sensibilidad"
+  "Tu alegría",
+  "Tu inteligencia",
+  "Tu hermosa sonrisa",
+  "Tu sentido del humor",
+  "Tu mirada",
+  "Tu forma de hablar y expresarte",
+  "Tu ternura",
+  "Tu sensibilidad"
 ];
